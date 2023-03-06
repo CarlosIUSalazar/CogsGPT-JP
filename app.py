87822fc488dd96d53b0b919de9241969c6dedefd
Carlos Salazar <carlosiusalazar@gmail.com>
2023-03-06 19:36:52 +0900

    changed ports

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def submit_form():
         return "Error calling OpenAI API"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
